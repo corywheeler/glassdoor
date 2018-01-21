0c70ad3a7a1cf6e92729c438ba8c123d249080a3
Cory Wheeler <corywheeler@gmail.com>
2018-01-20 22:24:58 -0500

    Filter Glassdoor results to Alpharetta
    When Glassdoor searched for Alpharetta from it's company search screen
    it actually searches all of metro Atlanta. On the search results screen
    you then have to filter it to Alpharetta, GA.
    
    This step will log out all of the Alpharetta companies on the first
    page of it's result set.

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var glance = new Glance({
     driverConfig: { desiredCapabilities: {browserName: 'chrome'} }
 });
 
-// As best I can tell, GlassDoor is finding where I am and setting location automatically
+// As best I can tell, GlassDoor is finding where I am and setting location automatically on it's Company Reviews page.
 // For now I'm going to rely on this as if I try to set the location, I get "Alpharetta, GAAlpharetta, GA"
 // and I'm not sure why. So for now, rely on the behavior I'm seeing.
 
 
-glance.url('http://glassdoor.com/')
+let companies = glance.url('http://glassdoor.com/')
     .click('Company Reviews#1')
-    .click('Search company reviews and ratings > button');
+    .click('Search company reviews and ratings > button')
+    .click('LocationFilter')
+    .click('Alpharetta, GA#1')
+    .get('.eiHdrModule.module.snug');
+
+console.log(companies)
